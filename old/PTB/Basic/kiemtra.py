49a2c24aa60bc2fcc7c1b477f3c53b1604f99dea
Khoailangngao-675 <nguyennhukhoa41@gmail.com>
2026-09-20 11:03:22 +0700

    20/09/2026

--- old/PTB/Basic/kiemtra.py
+++ old/PTB/Basic/kiemtra.py
@@ -1,12 +1,3 @@
-L,K = list(map(int,input().split()))
-N = input()
-
-for i in "QWERTYUIOPASDFGHJKLZXCVBNM":
-    d=0
-    for j in range(L):
-        if N[j] == i :
-            d += 1
-        if d >= K:
-            print(i,end="")
-            break
-        
+o = "*"
+i = 5
+print(o*i)
